e1d8c9acd83ce62e87057a85664cc2fcc8d57304
Sophie Sun <swl381029812@gmail.com>
2023-02-04 17:24:58 +1100

    add comment

--- StylingTodoList/src/components/UI/Button/Button.js
+++ StylingTodoList/src/components/UI/Button/Button.js
@@ -21,7 +21,7 @@ const Button = styled.button`
   }
 `
 
-// const Button = props => {
+// const Button = (props) => {
 //   return (
 //     <button type={props.type} className="button" onClick={props.onClick}>
 //       {props.children}
