7d20f4d5dcc578c9924d1a9c80f44ad0ce42145c
Sophie Sun <swl381029812@gmail.com>
2023-02-10 10:43:43 +1100

    add input

--- AddUsersPorject/src/components/Users/AddUser.js
+++ AddUsersPorject/src/components/Users/AddUser.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Button from '../UI/Button';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 
-const AddUser = () => {
+const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
   const addUserHandler = (e) => {
     // prevent the page reloading when click submit
     e.preventDefault();
+    // console.log(enteredUsername, enteredAge)
+  }
+  const inputNameHandler = (e) => {
+    setEnteredUsername(e.target.value);
+  }
+  const inputAgeHandler = (e) => {
+    setEnteredAge(e.target.value);
   }
 
 
@@ -13,10 +23,10 @@ const AddUser = () => {
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor='username'>Username</label>
-        <input id="username" type="text"/>
+        <input id="username" type="text" onChange={inputNameHandler}/>
         <label htmlFor='age'>Age</label>
-        <input id="age" type="number"/>
-        <button type='submit'>Add User</button>
+        <input id="age" type="number" onChange={inputAgeHandler}/>
+        <Button type='submit'>Add User</Button>
       </form>
     </Card>
   )
