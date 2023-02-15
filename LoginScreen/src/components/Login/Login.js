7797f975a502ef6f9eaf43e97506f7408749f98e
Sophie Sun <swl381029812@gmail.com>
2023-02-15 13:41:33 +1100

    update useEffect

--- LoginScreen/src/components/Login/Login.js
+++ LoginScreen/src/components/Login/Login.js
@@ -12,9 +12,17 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    const timeoutHandler = setTimeout(() => {
+      console.log('checking validity');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+    return () => {
+      // Clear the old timer everytime before new SetTimeout function run
+      console.log('cleanup');
+      clearTimeout(timeoutHandler)
+    }
   // depenencies should include things will change, setFormIsValid is only triggered if either of these changed
   },[enteredEmail, enteredPassword])
 
