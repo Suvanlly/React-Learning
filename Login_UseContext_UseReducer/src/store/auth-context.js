b7868be5818f1782948375cf34f8f1af7fe93b1a
Sophie Sun <swl381029812@gmail.com>
2023-02-20 10:03:49 +1100

    init

--- Login_UseContext_UseReducer/src/store/auth-context.js
+++ Login_UseContext_UseReducer/src/store/auth-context.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 // AuthContext is an object that contains component
 const AuthContext = React.createContext({
-  isLoggedIn: false
+  isLoggedIn: false,
+  // because in app.js we added onLogout in AuthContext.Provider, we added here as dummy function
+  onLogout: () => {}
 });
 
 export default AuthContext;
