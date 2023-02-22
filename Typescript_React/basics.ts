d10a8361fe3a570fc57b3596182d0c46da5318b7
Sophie Sun <swl381029812@gmail.com>
2023-02-22 19:37:10 +1100

    add notes

--- Typescript_React/basics.ts
+++ Typescript_React/basics.ts
@@ -11,3 +11,33 @@ age = 12;
 let userName: string;
 
 userName: 'abc';
+
+
+// More complex types
+
+let hobbies: string[]; // this means array of strings
+
+hobbies = ['Sports', 'Cooking'];
+
+// this should be avoid
+let person: any;
+
+person = {
+  name: 'Max',
+  age: 32
+}
+
+let person_specific: {
+  name: string,
+  age: number,
+}
+
+person_specific = {
+  name: 'sophie',
+  age: 30
+}
+
+// Union types (allow multiple types)
+let course: string | number = 'react';
+// now it won't have the error
+course = 1234;
