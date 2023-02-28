976c3a078be20a45ff88c43458c6fbb0c58bca38
Sophie Sun <swl381029812@gmail.com>
2023-02-28 16:44:51 +1100

    add todos

--- Typescript_React/Typescript_app/src/App.tsx
+++ Typescript_React/Typescript_app/src/App.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import {useState} from 'react';
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './data_models/todo';
 
 function App() {
-  const todos = [
-    new Todo('Learn React'),
-    new Todo('Learn TypeScript')
-  ];
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  // Hardcoded Todo List items
+  // const todos = [new Todo('Learn React'), new Todo('Learn TypeScript')];
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    })
+  }
+
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler}/>
       <Todos items={todos}/>
     </div>
   );
--- Typescript_React/Typescript_app/src/components/NewTodo.tsx
+++ Typescript_React/Typescript_app/src/components/NewTodo.tsx
@@ -1,8 +1,29 @@
-const NewTodo = () => {
+import React, { useRef } from "react";
+
+const NewTodo: React.FC<{onAddTodo: (text: string) => void }> = (props) => {
+  // The initial value null must be provided
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+  // FormEvent is a special Event Object Type when listening to form submission
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    // '?.' is for TS not find a value yet, it will return Null (can avoid error)
+    // '!.' is for TS to know we are sure it can't be Null
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText?.trim().length === 0) {
+      // throw an error
+      return;
+    }
+
+    props.onAddTodo(enteredText);
+  }
+
   return (
-    <form>
-      <label>Todo text</label>
-      <input type='text' />
+    <form onSubmit={submitHandler}>
+      <label htmlFor="text">Todo text</label>
+      <input type='text' id="text" ref={todoTextInputRef}/>
       <button>Add Todo</button>
     </form>
   )
