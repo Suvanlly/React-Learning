ed94b3362b1935f35b236ef03f13382285b40919
Sophie Sun <swl381029812@gmail.com>
2023-03-06 10:45:28 +1100

    comment

--- reactTesting/testing-cases/src/App.test.js
+++ reactTesting/testing-cases/src/App.test.js
@@ -1,1 +1,17 @@
 import '@testing-library/jest-dom';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("renders greeting text", () => {
+  render(<App />);
+  const greetingText = screen.getByText(/It's good to see you!/i);
+  expect(greetingText).toBeInTheDocument();
+});
+
+test("clicking on the button changes the greeting text", () => {
+  render(<App />);
+  const button = screen.getByRole("button");
+  button.click();
+  const changedText = screen.getByText(/Changed!/i);
+  expect(changedText).toBeInTheDocument();
+});
--- reactTesting/testing-cases/src/components/Async.test.js
+++ reactTesting/testing-cases/src/components/Async.test.js
@@ -18,7 +18,11 @@ describe('Async component', () => {
     expect(listItemElements).not.toHaveLength(0)
   });
 
+  // In order to test, we need to mock the fetch function, which is a global function available in the browser environment,
+  // and return some mock data that we can use to verify that our component is rendering the data correctly.
   test("renders list of posts fetched asynchronously", async () => {
+    /* We start by creating some mock data that we want to return when the fetch function is called. 
+    This mock data represents an array of posts, each with an id, title, body, and userId property.*/
     const mockPosts = [
       {
         userId: 1,
@@ -33,11 +37,16 @@ describe('Async component', () => {
         body: "Mock Post 2 Body",
       },
     ];
+    /* Next, we use Jest's spyOn function to mock the fetch function globally. 
+    We then mock the json method of the Response object that is returned by the fetch function to return our mock data. */
     jest.spyOn(global, "fetch").mockResolvedValue({
       json: jest.fn().mockResolvedValue(mockPosts),
     });
   
+    // We import the Async component itself that we want to test.
     render(<Async />);
+    // We can use screen.findByRole to wait for the list element to appear, since it is fetched asynchronously. 
+    // Once the list is rendered, we can verify that it contains the correct number of items and that each item has the correct title.
     const postList = await screen.findByRole("list");
     expect(postList).toBeInTheDocument();
   
@@ -46,6 +55,8 @@ describe('Async component', () => {
       expect(postTitle).toBeInTheDocument();
     });
   
+    // we restore the original fetch function using mockRestore to clean up our test environment.
+    // By mocking the fetch function and returning mock data, we can verify that our Async component correctly fetches and renders data from an API.
     global.fetch.mockRestore();
   });
 });
