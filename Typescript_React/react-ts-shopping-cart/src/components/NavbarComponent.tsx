60afdb8a02a7fac7bb5de2429d6939077af95f54
Sophie Sun <swl381029812@gmail.com>
2023-05-02 13:00:49 +1000

    add styles

--- Typescript_React/react-ts-shopping-cart/src/components/NavbarComponent.tsx
+++ Typescript_React/react-ts-shopping-cart/src/components/NavbarComponent.tsx
@@ -4,11 +4,12 @@ export function NavbarComponent() {
   return (
     <Navbar className="bg-white shadow-sm mb-3">
       <Container>
-        <Nav>
+        <Nav className="me-auto">
           <Nav.Link href="/" as={NavLink}>Home</Nav.Link>
           <Nav.Link href="/store" as={NavLink}>Store</Nav.Link>
           <Nav.Link href="/about" as={NavLink}>About</Nav.Link>
         </Nav>
+        Hi
       </Container>
     </Navbar>
   )
