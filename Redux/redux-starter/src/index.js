c1e71166c580fa4b3bca95bc65d08e6ee16e0838
Sophie Sun <swl381029812@gmail.com>
2023-05-23 12:02:39 +1000

    Add redux

--- Redux/redux-starter/src/index.js
+++ Redux/redux-starter/src/index.js
@@ -1,1 +1,22 @@
+import store from "./store";
+import * as actions from './actionTypes';
+
+store.subscribe(() => {
+  console.log("Store changed!", store.getState());
+})
+
+store.dispatch({
+  type: actions.BUG_ADDED,
+  payload: {
+    description: "Bug1"
+  }
+});
+
+store.dispatch({
+  type: actions.BUG_REMOVED,
+  payload: {
+    id: 1
+  }
+});
+
 console.log("Hello World!");
---
+++ Redux/redux-starter/src/reducer.js
@@ -0,0 +1,22 @@
+import * as actions from './actionTypes';
+// []
+let lastId = 0;
+
+// In redux, reducer has to be pure
+export default function reducer(state = [], action) {
+  if (action.type === actions.BUG_ADDED)
+    return [
+      ...state,
+      {
+        id: ++lastId,
+        description: action.payload.description,
+        resolved: false
+      }
+    ];
+    
+  else if (action.type === actions.BUG_REMOVED)
+    return state.filter(bug => bug.id !== action.payload.id)
+
+    // if the type of action is neither of above
+    return state;
+}
